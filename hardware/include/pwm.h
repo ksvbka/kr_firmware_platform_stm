ccc87588d74db0e7bd5ecebebd08930449aa792b
Kienltb <kien@seespace.co>
2016-12-16 11:33:04 +0700

    Refactor PWM module

--- hardware/include/pwm.h
+++ hardware/include/pwm.h
@@ -13,6 +13,7 @@
  *     - The Channel 2 output is GPIOA_9
  *     - The Channel 3 output is GPIOA_10
  *     - The Channel 4 output is GPIOA_11
+ * Duty cycle canbe set in range [0..1000]
  */
 
 #ifndef __PWM_H__
@@ -20,10 +21,10 @@
 
 #include "stm32f0xx.h"
 
-#define PWM_CHANNEL_1 (0x01)
-#define PWM_CHANNEL_2 (0x02)
-#define PWM_CHANNEL_3 (0x04)
-#define PWM_CHANNEL_4 (0x08)
+#define PWM_CHANNEL_1 (0x01)    /* Output on PA8*/
+#define PWM_CHANNEL_2 (0x02)    /* Output on PA9*/
+#define PWM_CHANNEL_3 (0x04)    /* Output on PA10*/
+#define PWM_CHANNEL_4 (0x08)    /* Output on PA11*/
 
 /* Init pwm module, channels canbe init as multi channel.
 *  Eg:
