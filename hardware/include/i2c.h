703c305ac016de8c59d8602fc05279a63f0eaf95
ksvbka <letrungkien.k53.hut@gmail.com>
2016-12-19 22:18:43 +0700

    Fix i2c module

--- hardware/include/i2c.h
+++ hardware/include/i2c.h
@@ -2,7 +2,7 @@
 * @Author: Trung Kien
 * @Date:   2016-11-29 11:43:01
 * @Last Modified by:   ksvbka
-* @Last Modified time: 2016-12-12 00:39:47
+* @Last Modified time: 2016-12-16 21:05:45
 */
 #ifndef __I2C_HW__H__
 #define __I2C_HW__H__
@@ -11,21 +11,28 @@
 #include "gpio.h"
 /**
  * STM32F0 has 2 I2C module is I2C1 and I2C2.
- * I2C1 pin canbe (SCL, SDA, AF number) mapping as folow
- *     - (PA9, PA10, AF4) (PA11, PA12, AF5)(PB6, PB7, AF1)  (PB8, PB9, AF1)
- * I2C2 pin canbe (SCL, SDA, AF number) mapping as folow
- *     - (PB10, PB11, AF1*), (PB13, PB14, AF5)
+ * I2C pin canbe config as folow:
+ *         I2C1 CLK:    PB6 AF1         PB8 AF1
+ *         I2C1 SDA:    PB7 AF1         PB9 AF1
+ *
+ *         I2C2 CLK:    PB10 AF1        PF6 AF
+ *         I2C2 SDA:    PB11 AF1        PF7 AF
  */
+
+
+
 #define I2C_MODULE_1 0x01
 #define I2C_MODULE_2 0x02
 
 /* Configure default  of I2C1 is PB8, PB9 - Fixed, not change*/
-#define I2C1_SCL_PIN (GPIO_PIN(GPIO_PB, 8))
-#define I2C1_SDA_PIN (GPIO_PIN(GPIO_PB, 9))
+#define I2C1_SCL_PIN    GPIO_PIN(GPIO_PB, 8)
+#define I2C1_SDA_PIN    GPIO_PIN(GPIO_PB, 9)
+#define I2C1_FUNCTION   AF1
 
-/* Configure default  of I2C2 is PB13, PB14 - Fixed, not change */
-#define I2C2_SCL_PIN (GPIO_PIN(GPIO_PB, 13))
-#define I2C2_SDA_PIN (GPIO_PIN(GPIO_PB, 14))
+/* Configure default  of I2C2 is PB10, PB11 - Fixed, not change */
+#define I2C2_SCL_PIN    GPIO_PIN(GPIO_PB, 10)
+#define I2C2_SDA_PIN    GPIO_PIN(GPIO_PB, 11)
+#define I2C2_FUNCTION   AF1
 
 
 /* Init module I2C (canbe I2C_MODULE_1 or I2C_MODULE_2)*/
--- hardware/src/i2c.c
+++ hardware/src/i2c.c
@@ -1,209 +1,119 @@
 #include "i2c.h"
 #include "stm32f0xx.h"
+#include "gpio.h"
+
+/* Debug */
+#include "uart.h"
+
 
 static I2C_TypeDef* I2Cx = NULL;
 
 void i2c_init(uint8_t i2c_module)
 {
-        /* Enable GPIOB clocks*/
-        RCC_APB2PeriphClockCmd ( RCC_AHBPeriph_GPIOB , ENABLE );
-
         /* Configure I2C clock and GPIO*/
-        GPIO_InitTypeDef GPIO_InitStructure ;
-        GPIO_StructInit(&GPIO_InitStructure );
-
-        if (i2c_module & I2C_MODULE_1) {
+        if (i2c_module == I2C_MODULE_1) {
                 I2Cx = I2C1;
-                /* I2C1 clock enable */
-                RCC_APB1PeriphClockCmd ( RCC_APB1Periph_I2C1 , ENABLE );
+                /* Enable i2c1 clock, reset i2c1*/
+                RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+                RCC->APB1RSTR |= RCC_APB1RSTR_I2C1RST;
+                RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C1RST;
 
-                /* I2C1 SDA and SCL configuration */
-                GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 ;
-                GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz ;
-                GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF ;
-                GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-                GPIO_InitStructure.GPIO_OType = GPIO_OType_OD; /*Open drain*/
-                GPIO_Init (GPIOB , &GPIO_InitStructure );
+                // i2c1 clock source == sysclk == 48mhz
+                RCC->CFGR3 |= RCC_CFGR3_I2C1SW;
 
-                GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_1);
-                GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_1);
+                // setup timing for 400hz fast mode
+#define PRESC   15
+#define SCLDEL  1
+#define SDADEL  1
+#define SCLH    14
+#define SCLL    14
+                I2Cx->TIMINGR = (SCLL << 0) | (SCLH << 8) | (SDADEL << 16) | (SCLDEL << 20) | (PRESC << 28);
 
-                /* I2C1 Reset */
-                RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1 , ENABLE );
-                RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1 , DISABLE );
+                I2Cx->CR1 |= 1; // enable periph
+                /* Configure gpio as i2c function*/
+                gpio_init_function(I2C1_SCL_PIN, I2C1_FUNCTION, OPEN_DRAIN, NO_PULL);
+                gpio_init_function(I2C1_SDA_PIN, I2C1_FUNCTION, OPEN_DRAIN, NO_PULL);
         }
 
-        if (i2c_module & I2C_MODULE_2) {
-                I2Cx = I2C2;
-                /* I2C2 clock enable */
-                RCC_APB1PeriphClockCmd ( RCC_APB1Periph_I2C2 , ENABLE );
-                /* I2C2 SDA and SCL configuration */
-                GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 ;
-                GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz ;
-                GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-                GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-                GPIO_InitStructure.GPIO_OType = GPIO_OType_OD; /*Open drain*/
-                GPIO_Init (GPIOB , &GPIO_InitStructure );
+        if (i2c_module == I2C_MODULE_2) {
+                // I2Cx = I2C2;
+                // /* I2C2 clock enable */
+                // RCC_APB1PeriphClockCmd ( RCC_APB1Periph_I2C2 , ENABLE );
+                // /* Reset I2C module */
+                // RCC->APB1RSTR |= RCC_APB1RSTR_I2C2RST;
+                // RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C2RST;
 
-                GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, GPIO_AF_5);
-                GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_5);
-
-                /* I2C2 Reset */
-                RCC_APB1PeriphResetCmd( RCC_APB1Periph_I2C2 , ENABLE );
-                RCC_APB1PeriphResetCmd( RCC_APB1Periph_I2C2 , DISABLE );
+                // /* Configure gpio as i2c function*/
+                // gpio_init_function(I2C2_SCL_PIN, I2C2_FUNCTION, OPEN_DRAIN, NO_PULL);
+                // gpio_init_function(I2C2_SDA_PIN, I2C2_FUNCTION, OPEN_DRAIN, NO_PULL);
         }
-
-        /* Configure I2Cx*/
-        I2C_InitTypeDef I2C_InitStructure;
-
-        I2C_StructInit(&I2C_InitStructure);
-        I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
-        I2C_InitStructure.I2C_AnalogFilter = I2C_AnalogFilter_Enable;
-        I2C_InitStructure.I2C_DigitalFilter = 0;
-        I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
-        I2C_InitStructure.I2C_OwnAddress1 = 0xAB;
-        I2C_InitStructure.I2C_Timing = 0x10805E89;  //0x50330309; // 400kHz at 48Mhz //0x1045061D
-        I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-
-        I2C_Init(I2Cx , &I2C_InitStructure );
-        I2C_Cmd(I2Cx , ENABLE );
 }
+
 
 uint8_t i2c_read_byte (uint8_t device_addr, uint8_t reg_addr)
 {
         int8_t data = 0;
 
-        /* Wait until I2C peripheral isn't busy!*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY) == SET);
+        I2Cx->CR2 = (device_addr << 1) | (1 << 16); // 1 byte transfer
+        I2Cx->CR2 |= I2C_CR2_START;
 
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+        while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+        I2Cx->TXDR = (reg_addr | (1 << 7)); // 7 LSBs = reg address, MSB=1 auto-increment
 
-        /* Wait until the transmit interrupt status is set*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
-
-        /* Send the address of the register you wish to read*/
-        I2C_SendData(I2Cx, (uint8_t)reg_addr);
-
-        /* Wait until transfer is complete!*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TC) == RESET);
-
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
-
-        data = I2C_ReceiveData(I2Cx);
-
-        /* Wait for the stop condition to be sent*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF) == RESET);
-
-        /* Clear the stop flag for next transfers*/
-        I2C_ClearFlag(I2Cx, I2C_FLAG_STOPF);
+        while ((I2Cx->ISR & I2C_ISR_TC) == 0); // wait for write to complete
+        I2Cx->CR2 = (device_addr << 1) | I2C_CR2_RD_WRN | (1 << 16);
+        I2Cx->CR2 |= I2C_CR2_START;
+        while ((I2Cx->ISR & I2C_ISR_RXNE) == 0); // wait for byte received
+        data = I2Cx->RXDR;
 
         return data;
 }
 
 uint8_t i2c_read_data (uint8_t device_addr, uint8_t reg_addr, uint8_t *rx_buffer, uint8_t length)
 {
-        /* Wait until I2C peripheral isn't busy!*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY) == SET);
+        I2Cx->CR2 = (device_addr << 1) | (1 << 16); // 1 byte transfer
+        I2Cx->CR2 |= I2C_CR2_START;
 
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+        while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+        I2Cx->TXDR = (reg_addr | (1 << 7)); // 7 LSBs = reg address, MSB=1 auto-increment
 
-        /* Wait until the transmit interrupt status is set*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
+        while ((I2Cx->ISR & I2C_ISR_TC) == 0); // wait for write to complete
+        I2Cx->CR2 = (device_addr << 1) | I2C_CR2_RD_WRN | (length << 16);
+        I2Cx->CR2 |= I2C_CR2_START;
 
-        /* Send the address of the register you wish to read*/
-        I2C_SendData(I2Cx, (uint8_t)reg_addr);
-
-        /* Wait until transfer is complete!*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TC) == RESET);
-
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, length, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
-
-        int8_t i;
-        for (i = 0; i < length; i++) {
-                /* Wait until the RX register is full of luscious data!*/
-                while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_RXNE) == RESET);
-                rx_buffer[i] = I2C_ReceiveData(I2Cx);
+        int i;
+        for (i = 0; i < length; ++i) {
+                while ((I2Cx->ISR & I2C_ISR_RXNE) == 0);
+                rx_buffer[i] = I2Cx->RXDR;
         }
-
-        /* Wait until STOPF flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF) == RESET);
-
-        /* Clear STOPF flag */
-        I2C_ClearFlag(I2Cx, I2C_FLAG_STOPF);
-
         return i;
 }
 
 uint8_t i2c_write_byte(uint8_t device_addr, uint8_t reg_addr, uint8_t data)
 {
-        /*Wait until I2C isn't busy*/
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY) == SET);
+        I2Cx->CR2 = (device_addr << 1) | I2C_CR2_AUTOEND | ((1 + 1) << 16);
+        I2Cx->CR2 |= I2C_CR2_START;
 
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_Reload_Mode, I2C_Generate_Start_Write);
+        while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+        I2Cx->TXDR = reg_addr | (1 << 7); // MSB set = auto-increment
 
-        /* Wait until TXIS flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
-
-        /* Send Register address */
-        I2C_SendData(I2Cx, reg_addr);
-
-        /* Wait until TCR flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TCR) == RESET);
-
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_AutoEnd_Mode, I2C_No_StartStop);
-
-        /* Wait until TXIS flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
-
-        /* Write data to TXDR */
-        I2C_SendData(I2Cx, data);
-
-        /* Wait until STOPF flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF) == RESET);
-
-        /* Clear STOPF flag */
-        I2C_ClearFlag(I2Cx, I2C_FLAG_STOPF);
-
+        while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+        I2Cx->TXDR = data;
         return 1;
 }
 
 uint8_t i2c_write_data(uint8_t device_addr, uint8_t reg_addr, uint8_t * tx_buffer, uint8_t length)
 {
-        //Wait until I2C isn't busy
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY) == SET);
+        I2Cx->CR2 = (device_addr << 1) | I2C_CR2_AUTOEND | ((length + 1) << 16);
+        I2Cx->CR2 |= I2C_CR2_START;
 
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, 1, I2C_Reload_Mode, I2C_Generate_Start_Write);
+        while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+        I2Cx->TXDR = reg_addr | (1 << 7); // MSB set = auto-increment
 
-        /* Wait until TXIS flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
-
-        /* Send Register address */
-        I2C_SendData(I2Cx, reg_addr);
-
-        /* Wait until TCR flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TCR) == RESET);
-
-        /* Configure slave address, nbytes, reload, end mode and start or stop generation */
-        I2C_TransferHandling(I2Cx, device_addr, length, I2C_AutoEnd_Mode, I2C_No_StartStop);
         int i;
         for (i = 0; i < length; ++i) {
-                /* Wait until TXIS flag is set */
-                while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_TXIS) == RESET);
-                /* Write data to TXDR */
-                I2C_SendData(I2Cx, tx_buffer[i]);
+                while ((I2Cx->ISR & I2C_ISR_TXIS) == 0); // wait for empty tx buffer
+                I2Cx->TXDR = tx_buffer[i];
         }
-
-        /* Wait until STOPF flag is set */
-        while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF) == RESET);
-
-        /* Clear STOPF flag */
-        I2C_ClearFlag(I2Cx, I2C_FLAG_STOPF);
-
         return i;
 }
