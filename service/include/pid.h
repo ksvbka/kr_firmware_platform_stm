6cd50a41693e8132cc0e6340e195662c825b79ad
ksvbka <letrungkien.k53.hut@gmail.com>
2016-12-25 17:19:50 +0700

    Fixed error when cast pwm as int

--- service/include/pid.h
+++ service/include/pid.h
@@ -1,8 +1,8 @@
 /*
 * @Author: Kienltb
 * @Date:   2016-12-22 14:27:49
-* @Last Modified by:   Kienltb
-* @Last Modified time: 2016-12-22 14:42:24
+* @Last Modified by:   ksvbka
+* @Last Modified time: 2016-12-25 17:11:11
 */
 
 /**
@@ -25,12 +25,12 @@ typedef struct PID{
         float out_min;
         float out_max;
         float last_input;
-        float last_process_value;
-        float sample_time; /*ms*/
+        float error_accumulate;
+        float sample_time; /*s*/
         bool  direction; /* PID_DIRECT or PID_REVERSE*/
 }PID_t;
 
-void pid_set_param(PID_t* pid, float kp, float ki, float kd, bool direction);
+void pid_set_param(PID_t* pid, float kp, float ki, float kd, bool direction, float sample_time);
 
 float pid_compute(PID_t* pid, float setpoint, float input);
 
--- service/src/pid.c
+++ service/src/pid.c
@@ -1,19 +1,18 @@
 /*
 * @Author: Trung Kien
 * @Date:   2016-11-29 11:33:44
-* @Last Modified by:   Kienltb
-* @Last Modified time: 2016-12-22 17:59:41
+* @Last Modified by:   ksvbka
+* @Last Modified time: 2016-12-25 17:11:45
 */
 
 #include "pid.h"
 
-void pid_set_param(PID_t* pid, float kp, float ki, float kd, bool direction)
+void pid_set_param(PID_t* pid, float kp, float ki, float kd, bool direction, float sample_time)
 {
-        float sample_time_inSec = pid->sample_time / 1000.0;
-
         pid->kp = kp;
-        pid->ki = ki * sample_time_inSec;
-        pid->kd = kd / sample_time_inSec;
+        pid->ki = ki;
+        pid->kd = kd;
+        pid->sample_time = sample_time;
 
         if (direction == PID_REVERSE) {
                 pid->kp = -pid->kp;
@@ -24,20 +23,15 @@ void pid_set_param(PID_t* pid, float kp, float ki, float kd, bool direction)
 
 float pid_compute(PID_t* pid, float set_point, float input)
 {
-        float error, d_input, output;
+        float error = set_point - input;
+        float d_input = input - pid->last_input;
+        pid->error_accumulate += error;
 
-        error = set_point - input;
+        float p_out = pid->kp * error;
+        float i_out = pid->ki*pid->error_accumulate *pid->sample_time;
+        float d_out = - (pid->kd * d_input)/pid->sample_time;
 
-        pid->last_process_value += (pid->ki * error);
-
-        if (pid->last_process_value > pid->out_max)
-                pid->last_process_value = pid->out_max;
-
-        if (pid->last_process_value < pid->out_min)
-                pid->last_process_value = pid->out_min;
-
-        d_input = input - pid->last_input;
-        output = pid->kp * error + pid->last_process_value - pid->kd * d_input;
+        float output = p_out + i_out + d_out;
 
         if (output > pid->out_max)
                 output = pid->out_max;
