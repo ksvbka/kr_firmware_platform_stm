92bc06f18851aa9b63aee1b639abf7489001d8e7
Kienltb <kien@seespace.co>
2017-01-09 16:32:39 +0700

    Add utility function

--- hardware/src/utility.c
+++ hardware/src/utility.c
@@ -1,15 +1,13 @@
 /*
 * @Author: Trung Kien
 * @Date:   2016-11-29 11:43:01
-* @Last Modified by:   ksvbka
-* @Last Modified time: 2016-12-12 00:39:47
+* @Last Modified by:   Kienltb
+* @Last Modified time: 2017-01-09 16:31:58
 */
 
 #include "utility.h"
 
 /* Helper function */
-
-static char buf[10];
 
 /* Convert String to Integer*/
 int to_int(char *s)
@@ -52,4 +50,16 @@ uint32_t u32ToLittleEndian(uint32_t u32input )
                  ^ ( (u32input << 8) & 0x00FF0000 )
                  ^ ( (u32input << 24) & 0xFF000000 )
                );
+}
+
+/* Convert a float into 4byte and store to buffer */
+void float_to_buff(float data, char* buffer)
+{
+        buffer = (char*)(&data);
+}
+
+/* Convert a 4byte in buffer to float */
+float buffer_to_float(char* buffer)
+{
+        return *(float*)buffer;
 }
