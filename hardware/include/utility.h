8d123afcd12a40119d6a1865b3d7ad07426d740e
Kienltb <kien@seespace.co>
2017-01-10 16:28:38 +0700

    update ultility

--- hardware/include/utility.h
+++ hardware/include/utility.h
@@ -2,7 +2,7 @@
 * @Author: Trung Kien
 * @Date:   2016-11-29 11:43:01
 * @Last Modified by:   Kienltb
-* @Last Modified time: 2017-01-09 16:17:39
+* @Last Modified time: 2017-01-09 16:22:17
 */
 
 #ifndef __ULTILITY_H__
@@ -30,5 +30,11 @@ uint16_t u16ToLittleEndian(uint16_t u16input );
 
 /* Use this function to convert a 32-bit number into little endian. */
 uint32_t u32ToLittleEndian(uint32_t u32input );
+
+/* Convert a float into 4byte and store to buffer */
+void float_to_buff(float data, char* buffer);
+
+/* Convert a 4byte in buffer to float */
+float buffer_to_float(char* buffer);
 
 #endif
